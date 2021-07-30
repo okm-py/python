4e7a697d872b097be20ac03f830323690e860dc1
okm-py <oktm625@gmail.com>
2021-07-30 17:43:30 +0900

    DjangoのSECRET_KEYをGitでの管理対象外にする

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&u%jt^4+s9o1z0wj^-#+ki(a0(*eg#cotoxt+k5(r4kqrkflh*'
+from .local_settings import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
