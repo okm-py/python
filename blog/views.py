9e96c8534325900a4d9dd8e9cc5b751a210fb3d0
okm-py <oktm625@gmail.com>
2021-10-29 15:45:33 +0900

    編集機能追加

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,17 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+# 記事一覧
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+# 記事詳細
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+# 記事投稿
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -22,4 +25,19 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+# 記事編集
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
